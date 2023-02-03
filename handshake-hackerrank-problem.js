f03d8aa33e7ea1a2d235cbabd7a93f5e4bd03fca
Shivam Uttam <102919037+Fadedrifleman@users.noreply.github.com>
2023-02-03 07:03:34 +0000

    completed asigment

---
+++ handshake-hackerrank-problem.js
@@ -0,0 +1,9 @@
+function handshake(n) {
+    return n * (n - 1) / 2;
+}
+console.log(handshake(1));
+console.log(handshake(2));
+console.log(handshake(4));
+console.log(handshake(5));
+console.log(handshake(6));
+console.log(handshake(7));
